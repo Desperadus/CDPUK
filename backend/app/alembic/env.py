656cc4c964d11c77bd19ca48daf488f73529e6fc
Tomas Jelinek <tomgolf.jelinek@gmail.com>
2024-08-08 21:27:08 +0200

    udpate and make env.py use dot env

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -1,8 +1,11 @@
 import os
+import sys
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+
+from dotenv import load_dotenv
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -12,6 +15,8 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
+path_for_env = os.path.join(os.path.dirname(__file__), "../../..", ".env")
+load_dotenv(dotenv_path=path_for_env)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
@@ -19,7 +24,7 @@ fileConfig(config.config_file_name)
 # target_metadata = None
 
 from app.models import SQLModel  # noqa
-from app.core.config import settings # noqa
+from app.core.config import settings  # noqa
 
 target_metadata = SQLModel.metadata
 
